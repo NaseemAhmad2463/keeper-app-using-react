4908745e08179b7a442b6bcda47085da68dea553
NaseemAhmad2463 <83572602+NaseemAhmad2463@users.noreply.github.com>
2022-12-24 17:49:18 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+function Footer(){ 
+    return <footer><p>Copyright {currentYear}</p></footer>
+}
+
+export default Footer;
